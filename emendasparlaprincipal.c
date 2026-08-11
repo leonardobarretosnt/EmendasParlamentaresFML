5a35a6f170b1c58822d65ed8af6a1146f25d6274
Leonardo Barreto <leonardo.brtsantos@gmail.com>
2026-08-11 15:44:40 -0300

    Commit final 2

--- emendasparlaprincipal.c
+++ emendasparlaprincipal.c
@@ -20,6 +20,7 @@ int main() {
     char autorMenor[TAM_AUTOR] = "";
     double maiorValor = -DBL_MAX;
     double menorValor = DBL_MAX;
+    int QtdEmendas = 0;
     int primeira = 1;
 
     fgets(linha, sizeof(linha), arquivo); // ignora cabecalho
@@ -47,8 +48,6 @@ int main() {
             continue;
         }
 
-        // 10 - estado
-        // 12 - valor com virgula
         char *virgula = strchr(colunas[12], ',');
         if (virgula != NULL) {
             *virgula = '.';
@@ -67,81 +66,20 @@ int main() {
             strncpy(autorMaior, colunas[2], TAM_AUTOR - 1);
             autorMaior[TAM_AUTOR - 1] = '\0';
         }
-        if (valor < menorValor) {
+        if (valor < menorValor && valor > 0) {
             menorValor = valor;
             strncpy(autorMenor, colunas[2], TAM_AUTOR - 1);
             autorMenor[TAM_AUTOR - 1] = '\0';
+
+            
         }
+        QtdEmendas++;
     }
 
     printf("%s eh o autor da maior emenda de R$ %.2lf\n", autorMaior, maiorValor);
     printf("%s eh o autor da menor emenda de R$ %.2lf\n", autorMenor, menorValor);
+    printf("%d eh a quantidade total de emendas", QtdEmendas);
 
     fclose(arquivo);
     return 0;
 }
-/*#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <locale.h>
-
-int main() {
-    FILE *arquivo = fopen("C:/Universidade/TrabalhoDeLincoln/EmendasParlamentaresFML/EmendasParlamentaresFML/EmendasParlamentares.csv", "r");
-    if (arquivo == NULL) {
-        printf("Erro ao abrir o arquivo csv!\n");
-        return 1;
-    }
-
-    char linha[2048];     
-    char *colunas[25];
-    char autorMaior[501];
-    char autorMenor[501];
-    double maiorValor = 0;
-    double menorValor = 0;
-    
-    fgets(linha, sizeof(linha), arquivo); //ignora cabecalho
-
-    while (fgets(linha, sizeof(linha), arquivo) != NULL) {
-        int i = 0;
-
-        char *token = strtok(linha, ";\n");
-
-        while (token != NULL) {
-            token++;
-            char *p = strchr(token, '\"');
-            *p = '\0';
-            colunas[i] = token;
-            i++;
-            token = strtok(NULL, ";\n");
-        }
-
-        //colunas estao prontas
-        //10 - estado
-        //12 - valor com virgula
-        
-        char *p = strchr(colunas[12], ',');
-        *p = '.';
-        double valor = strtod(colunas[12], NULL);
-        printf("%lf", valor);
-        if (valor > maiorValor){
-            maiorValor = valor;
-            strcpy(autorMaior, colunas[2]);
-            puts("A");
-            //printf("%s eh o autor da maior emenda de R$ %.2lf\n", autor, maiorValor);
-        }
-
-        if (valor < menorValor){
-            menorValor = valor;
-            strcpy(autorMenor, colunas[2]);
-            puts("B");
-            //printf("%s eh o autor da maior emenda de R$ %.2lf\n", autor, menorValor);
-        }
-    }
-
-    printf("%s eh o autor da maior emenda de R$ %.2lf\n", autorMaior, maiorValor);
-    printf("%s eh o autor da menor emenda de R$ %.2lf\n", autorMenor, menorValor);
-
-    fclose(arquivo);
-    return 0;
-}
-    */
